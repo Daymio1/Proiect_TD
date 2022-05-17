f5c7b71592ea2ee8ba9851082a9515cbe68b6707
Daymio1 <alexandru-razvanmuresan@outlook.com>
2022-05-18 00:35:33 +0300

    Frontend:Login+Register

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -2,3 +2,138 @@ import {useState, useEffect} from "react";
 import axios from "../api/axios";
 import {Alert, Form, Row} from 'react-bootstrap';
 import {Link, useNavigate, useLocation} from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+
+const LOGIN_URL = '/login';
+
+const Login = () => {
+    const { setAuth } = useAuth();
+
+    //const navigate = useNavigate();
+    // const location = useLocation();
+    // const from = location.state?.from?.pathname || "/dashboard";
+
+
+    const [username, setUsername] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [validated, setValidated] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+
+    /* Sterge erroarea in momentul in care user/pwd sunt modificate */
+    useEffect(() => {
+        setErrMsg('');
+    }, [username, pwd]);
+
+
+    /* Apeleaza API-ul cu datele din form */
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        }
+        setValidated(true);
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({username, pwd}),
+                {
+                    headers: {'Content-Type': 'application/json'},
+                    withCredentials: false
+                }
+            );
+            console.log(response);
+            const accessToken = response?.data?.accessToken;
+            //const role = response?.data?.role;
+            localStorage.setItem("token", "Bearer: "+ accessToken)
+            localStorage.setItem("username", username)
+            //localStorage.setItem("role", role)
+            localStorage.setItem("isAuth", "true");
+
+            setAuth({isAuth: true,username, pwd, accessToken})
+
+            setUsername('');
+            setPwd('');
+
+            //navigate(from, { replace: true });
+
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                console.log(err.response)
+                setErrMsg(err.response.data?.message || 'Login failed')
+            } else if (err.response?.status === 401) {
+                setErrMsg(err.response.data?.message || 'Login failed')
+            } else {
+                setErrMsg('Login Failed');
+            }
+        }
+    }
+    const renderErrorMessage = () => {
+        return (
+            <Alert variant="danger">
+                <p>{errMsg}</p>
+            </Alert>
+        )
+    }
+
+    const renderLoginForm = () => {
+        return (
+            <Form onSubmit={handleSubmit} validated={validated}>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label className="labelLogin">Username</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Username"
+                        autoComplete="off"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}/>
+                </Form.Group>
+
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className="labelLogin">Password</Form.Label>
+
+                    <Form.Control
+                        required
+                        type="password"
+                        placeholder="Password"
+                        aria-autocomplete={'none'}
+                        autoComplete="off"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}/>
+                </Form.Group>
+
+
+                <div className="btnLogInPosition">
+                    <button className="buttonLogin">Log In</button>
+                </div>
+                <div className="textLink" style={{textAlign: "center"}}>
+                    Need an account? <Link to="/register" className="link"> Sign in</Link>
+                </div>
+                <div>
+                    {errMsg ? renderErrorMessage() : ''}
+                </div>
+
+            </Form>
+        )
+    }
+
+    return (
+        <>
+            <div className="position-absolute rectangleLogin p-4">
+                <Row>
+                    {renderLoginForm()}
+                </Row>
+                <hr/>
+            </div>
+
+        </>
+    )
+}
+
+export default Login;
